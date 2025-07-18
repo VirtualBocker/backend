2df5cd0b0d7ca4a1e723879aa875e4f1d2c176ee
Ilya <burdgun@gmail.com>
2025-07-18 11:04:21 +0700

    created new handler for docker ps -a

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,14 +1,65 @@
 // Файл, который лежит в src/bin/*.rs образует crate-исполняемый файл (main)
 // Из main можно пользоваться только тем, что выставлено наружу (pub) из библиотечного crate и корректно объявлено в lib.rs
+use backend::lib::handlers::handler_return_all_containers;
 use backend::lib::req_res_structs::{BodyType, Response};
 use backend::lib::request::Request;
 use backend::lib::{http_server::Server, parse_funcs::deser_response};
 fn main() {
     let mut server = Server::new("127.0.0.1:8080").unwrap();
 
-    // server.POST("/container/:id/delete", |r: &Request| {
-    //     Response { response_code: 200, headers: None, body: None }
-    // });
+    // регистрация пары path и handlers в Hash-table
+    server.GET("/container/".to_string(), handler_return_all_containers); // 2ой аргумент это тип HandlerFn
+
+    server.POST("/container/:id/reboot".to_string(), |r: &Request| {
+        Response {
+            response_code: 200,
+            headers: None,
+            body: None,
+        }
+    });
+
+    server.POST("/container/:id/start".to_string(), |r: &Request| Response {
+        response_code: 200,
+        headers: None,
+        body: None,
+    });
+
+    server.POST("/container/:id/stop".to_string(), |r: &Request| Response {
+        response_code: 200,
+        headers: None,
+        body: None,
+    });
 
     server.start();
 }
+
+/* Как сейчас выглядит Hash-table
+handlers                          // HashMap<Method, …>
+│
+├── Method::GET ──┐     // 1‑й уровень
+│                 │
+│   +------------------------------------------------+  // 2‑й уровень (HashMap<String, HandlerFn>)
+│   |  "/container/" → handler_return_all_containers |
+│   +------------------------------------------------+
+│
+├── Method::POST ─┐     // 1‑й уровень
+│                 │
+│   +---------------------------------------------+ // 2‑й уровень (HashMap<String, HandlerFn>)
+│   | "/container/:id/reboot" → to_be_determined  |
+│   | "/container/:id/start" → to_be_determined   |
+│   | "/container/:id/stop" → to_be_determined    |
+│   +---------------------------------------------+
+│
+├── Method::PUT  ─┐
+│                 │
+│   +---------------------------+
+│   | EMPTY                     |
+│   +---------------------------+
+│
+└── Method::DELETE ─┐
+                  │
+    +---------------------------+
+    | EMPTY                     |
+    +---------------------------+
+
+*/
--- src/lib/request.rs
+++ src/lib/request.rs
@@ -8,7 +8,7 @@ pub struct Request {
     pub path: String,
     pub headers: Option<Vec<String>>, // Option - либо Some, либо None
     pub body: Option<BodyType>,       // Option - либо Some, либо None
-    pub rest_params: HashMap<String, String>
+    pub rest_params: HashMap<String, String>,
 }
 
 impl Default for Request {
@@ -18,21 +18,23 @@ impl Default for Request {
             path: String::default(),
             headers: None,
             body: None,
-            rest_params: HashMap::new()
+            rest_params: HashMap::new(),
         }
     }
 }
 
 impl Request {
     pub fn parse_args(&mut self, path: &str) {
-
         let request_chunks: Vec<&str> = self.path.split("/").collect();
 
         for (i, key_chunk) in path.split("/").enumerate() {
-            if i == 0 { continue; }
+            if i == 0 {
+                continue;
+            }
             if key_chunk.starts_with(":") {
                 let (_, id) = key_chunk.split_at(1);
-                self.rest_params.insert(id.to_string(), request_chunks[i].to_string());
+                self.rest_params
+                    .insert(id.to_string(), request_chunks[i].to_string());
             }
         }
     }
@@ -42,7 +44,9 @@ impl Request {
     pub fn is_exact(&mut self, path: &str) -> bool {
         let request_chunks: Vec<&str> = self.path.split("/").collect();
         for (i, key_chunk) in path.split("/").enumerate() {
-            if i == 0 { continue; }
+            if i == 0 {
+                continue;
+            }
             if !key_chunk.starts_with(":") && !(key_chunk == request_chunks[i]) {
                 return false;
             }
@@ -69,11 +73,12 @@ mod tests {
         };
 
         let mut expected_request = request.clone();
-        expected_request.rest_params.insert("id".to_string(), "label".to_string());
+        expected_request
+            .rest_params
+            .insert("id".to_string(), "label".to_string());
 
         request.parse_args(path);
         assert!(request.is_exact(path));
         assert_eq!(request, expected_request);
     }
-
 }
